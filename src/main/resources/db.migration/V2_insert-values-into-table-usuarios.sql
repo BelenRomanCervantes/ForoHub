42a59decffb37914bf4b78f79f35566446cf2bd7
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2025-01-15 13:13:55 -0600

    Created migrations

---
+++ src/main/resources/db.migration/V2_insert-values-into-table-usuarios.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios (nombre, email, password) VALUES ("belen roman", "belen.roman@correo.com", "123456");
+INSERT INTO usuarios (nombre, email, password) VALUES ("pablo herrera", "pablo89@correo.com", "456789");
+INSERT INTO usuarios (nombre, email, password) VALUES ("viridiana sanchez", "viri.sanchez@correo.com", "246810");
