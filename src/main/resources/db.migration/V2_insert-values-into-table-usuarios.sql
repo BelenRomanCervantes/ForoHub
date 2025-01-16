533cb30fec28ce1d5dfab68ab7782ebf4e935d93
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2025-01-16 17:43:04 -0600

    Created user authentication functionality

--- src/main/resources/db.migration/V2_insert-values-into-table-usuarios.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO usuarios (nombre, email, password) VALUES ("belen roman", "belen.roman@correo.com", "123456");
-INSERT INTO usuarios (nombre, email, password) VALUES ("pablo herrera", "pablo89@correo.com", "456789");
-INSERT INTO usuarios (nombre, email, password) VALUES ("viridiana sanchez", "viri.sanchez@correo.com", "246810");
